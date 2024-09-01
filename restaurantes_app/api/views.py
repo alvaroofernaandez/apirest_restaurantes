9ef90fd265056d16bc157974cfde3eabc26e468c
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-09-02 01:18:02 +0200

    métodos REST (GET,POST,PUT,DELETE) creados y probados

--- restaurantes_app/api/views.py
+++ restaurantes_app/api/views.py
@@ -1,26 +1,62 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 from .models import Restaurante
+import json
 
 
 # Aquí crearemos la vista:
 
 class RestaurantView(View):
 
-    def get(self, request):
-        restaurantes = list(Restaurante.objects.values())
-        if len(restaurantes)>0:
-            datos={'message': "Success", 'restaurantes': restaurantes}
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, requestm, id=0):
+        if (id>0):
+            restaurantes = list(Restaurante.objects.filter(id=id).values())
+            if len(restaurantes) > 0:
+                restaurante = restaurantes[0]
+                datos={'message': "Restaurantes encontrados!", 'restaurantes': restaurantes}
+            else:
+                datos = {'message': "Restaurants not found :( ..."}
+            return JsonResponse(datos)
         else:
-            datos={'message': "Restaurants not found :( ..."}
-        return JsonResponse(datos)
+            restaurantes = list(Restaurante.objects.values())
+            if len(restaurantes)>0:
+                datos={'message': "Restaurantes encontrados!", 'restaurantes': restaurantes}
+            else:
+                datos={'message': "Restaurants not found."}
+            return JsonResponse(datos)
 
     def post(self, request):
-        pass
+        jd = json.loads(request.body)
+        Restaurante.objects.create(name=jd['name'],web=jd['web'],yearFoundation=jd['yearFoundation'])
+        datos={'message': "Restaurante añadido con éxito."}
+        return JsonResponse(datos)
 
-    def put(self, request):
-        pass
 
-    def delete(self, request):
-        pass
+    def put(self, request, id):
+        jd = json.loads(request.body)
+        restaurantes = list(Restaurante.objects.filter(id=id).values())
+        if len(restaurantes) > 0:
+            restaurante = Restaurante.objects.get(id=id)
+            restaurante.name=jd['name']
+            restaurante.web=jd['web']
+            restaurante.yearFoundation=jd['yearFoundation']
+            restaurante.save()
+            datos = {'message': "Restaurante editado con éxito."}
+        else:
+            datos = {'message': "Restaurants not found..."}
+        return JsonResponse(datos)
+
+    def delete(self, request, id):
+        restaurantes = list(Restaurante.objects.filter(id=id).values())
+        if len(restaurantes) > 0:
+            Restaurante.objects.filter(id=id).delete()
+            datos = {'message': "Restaurante eliminado."}
+        else:
+            datos = {'message': "Restaurants not found..."}
+        return JsonResponse(datos)
