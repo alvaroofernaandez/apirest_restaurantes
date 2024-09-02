809505737955f3a3c3179caa2d22be850e02005f
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-09-02 02:41:44 +0200

    aprobación metodo CORS para poder conectar el frontend y traer el endpoint GET

--- restaurantes_app/restaurantes_app/settings.py
+++ restaurantes_app/restaurantes_app/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000/"]
